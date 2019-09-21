ae7dd3a7ab02dab148fa661098ebde76ade26ab0
roseline124 <guseod24@gmail.com>
2019-09-21 16:56:50 +0900

    javascript to typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
 /**
- * Configure your Gatsby site with this file.
- *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Gatsby`,
+    siteUrl: `https://www.gatsbyjs.org`,
+    description: `Blazing fast modern site generator for React`,
+  },
+  plugins: [
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <div><h1>Thanks, JinSeop Nim!</h1></div>
