9aa18557112cb155fa8011eb788ab993ed352849
roseline124 <guseod24@gmail.com>
2019-09-22 13:14:57 +0900

    add cover component

---
+++ src/components/Base.tsx
@@ -0,0 +1,6 @@
+// components to be used frequentlyin other components
+
+import React from 'react'
+import styled from 'styled-components'
+
+export const Container = styled.div``
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 
+import { Container } from '../components/Base'
+
 export default () => (
   <div>
     <Header></Header>
