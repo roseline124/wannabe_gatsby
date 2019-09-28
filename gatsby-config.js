e3a627b9b99c3d1fb8186f6d4e9e018ecabf75fd
roseline124 <guseod24@gmail.com>
2019-09-28 23:26:22 +0900

    add typography moraga theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
---
+++ src/utils/typography.ts
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+import moraga from 'typography-theme-moraga'
+
+const typography = Typography(moraga)
+
+export const { scale, rhythm, options } = typography
+export default typography
