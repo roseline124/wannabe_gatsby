7bd922a703dff235931b633c1a7d71eaed5aec98
roseline124 <guseod24@gmail.com>
2020-06-28 14:36:28 +0900

    Trim body layout

--- src/templates/BodyLayout.tsx
+++ src/templates/BodyLayout.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-import Header from '../components/Header'
+const defaultTitle = 'title'
+const defaultContent = 'content'
 
-export default ({ children }) => (
-  <div>
-    <Header />
-    {children}
-  </div>
-)
+const useStyles = makeStyles({
+  root: {
+    padding: '50px 0',
+  },
+})
+
+const BodyLayout = () => {
+  const classes = useStyles()
+
+  return (
+    <Box className={classes.root}>
+      <h1>{defaultTitle}</h1>
+      <h1>{defaultContent}</h1>
+    </Box>
+  )
+}
+
+export default BodyLayout
